4e834cef580e07209572e9a7a338fed6849b4a86
tayathorn <tayathorn@thinknet.co.th>
2017-11-09 16:50:41 +0700

    change cartoon data

--- src/data/cartoon.js
+++ src/data/cartoon.js
@@ -5,20 +5,6 @@ export const cartoon = [
     "premiere_date": "April 5, 2010",
     "current_season": 9,
     "profile_picture": "https://yt3.ggpht.com/-CSQcGxnR6vM/AAAAAAAAAAI/AAAAAAAAAAA/DLEXndGLfRI/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
-    "detail": {
-      "genre": [
-        "comedy",
-        "fantasy",
-        "adventure",
-        "science fiction"
-      ],
-      "origin": "United States",
-      "gallery": [
-        "https://www.geek.com/wp-content/uploads/2015/10/adventure_time.jpg",
-        "http://www.cartoonbrew.com/wp-content/uploads/2016/10/adventuretime_end.jpg",
-        "http://www.indiewire.com/wp-content/uploads/2016/09/adventuretime.jpg",
-      ]
-    }
   },
   {
     "id": 2,
@@ -26,20 +12,6 @@ export const cartoon = [
     "premiere_date": "November 4, 2013",
     "current_season": 5,
     "profile_picture": "https://vignette.wikia.nocookie.net/steven-universe/images/f/f0/Steven_Shield_WD.png/revision/latest/scale-to-width-down/350?cb=20160406110631",
-    "detail": {
-      "genre": [
-        "action",
-        "fantasy",
-        "comedy",
-        "drama",
-      ],
-      "origin": "United States",
-      "gallery": [
-        "https://www.gannett-cdn.com/-mm-/836ab7e4feff3cc85ce7f3f4123b1f621d3e917b/c=0-74-800-526&r=x803&c=1600x800/local/-/media/Wilmington/NG/2013/11/14/stevenuniverse.jpg",
-        "https://8fd3e791a58596c0ca97-e2039d154aebc68b9bb080249997e7ef.ssl.cf1.rackcdn.com/1774_cb6295f2.jpg",
-        "https://cdn.unicornbooty.com/wp-content/uploads/2017/07/16122947/steven-universe-racist.jpg"
-      ]
-    }
   },
   {
     "id": 3,
@@ -47,18 +19,6 @@ export const cartoon = [
     "premiere_date": "July 27, 2015",
     "current_season": 3,
     "profile_picture": "https://vignette.wikia.nocookie.net/parody/images/b/b9/We_bare_bears_charcters.jpg/revision/latest?cb=20150811054659",
-    "detail": {
-      "genre": [
-        "animation",
-        "comedy",
-      ],
-      "origin": "United States",
-      "gallery": [
-        "https://i.ytimg.com/vi/sX6WsoZHjc4/maxresdefault.jpg",
-        "https://pbs.twimg.com/media/C0etfRhXUAA2Fg7.jpg",
-        "https://i.ytimg.com/vi/sX6WsoZHjc4/maxresdefault.jpg",
-      ]
-    }
   },
   {
     "id": 4,
@@ -66,19 +26,6 @@ export const cartoon = [
     "premiere_date": "April 4, 2016",
     "current_season": 2,
     "profile_picture": "https://vignette.wikia.nocookie.net/powerpuff/images/a/a7/Tumblr_lv8big5jK51qizyws.jpg/revision/latest/scale-to-width-down/300?cb=20120508020045",
-    "detail": {
-      "genre": [
-        "action",
-        "adventure",
-        "comedy",
-      ],
-      "origin": "United States",
-      "gallery": [
-        "https://nerdist.com/wp-content/uploads/2017/09/powerpuff-girls.jpg",
-        "https://media.wired.com/photos/592715e2cfe0d93c474324eb/master/pass/PowerpuffGirls.jpg",
-        "https://pmctvline2.files.wordpress.com/2016/04/the-powerpuff-girls-08.jpg?w=620",
-      ]
-    }
   },
   {
     "id": 5,
@@ -86,18 +33,5 @@ export const cartoon = [
     "premiere_date": "April 10, 2017",
     "current_season": 1,
     "profile_picture": "https://vignette.wikia.nocookie.net/ben10/images/4/45/Rebootlogo.png/revision/latest?cb=20160916223147",
-    "detail": {
-      "genre": [
-        "action",
-        "adventure",
-        "comedy",
-        "supernatural",
-      ],
-      "origin": "United States",
-      "gallery": [
-        "https://pbs.twimg.com/media/C_aNMGDUMAA_FS1.jpg",
-        "https://i.ytimg.com/vi/LXWjT3_b278/maxresdefault.jpg"
-      ]
-    }
   }
 ]
